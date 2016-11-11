54bcaf0725bd095e75dfdc8b1ceae51f3c0cc251
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2016-11-11 17:28:56 +0100

    sufnituning genres with boolean variables

--- week-04/day-5/Jukebox/Song.h
+++ week-04/day-5/Jukebox/Song.h
@@ -9,10 +9,17 @@ public:
   void add_rating(float new_rating);
   float get_rating(int index);
   float get_average_rating();
+  void rock();
+  void pop();
+  void reggae();
+  std::string get_genre();
   ~Song();
 private:
   std::string title;
   std::string artist;
+  bool is_rock = false;
+  bool is_pop = false;
+  bool is_reggae = false;
   float rating;
   int rating_count = 0;
 };
--- week-04/day-5/Jukebox/main.cpp
+++ week-04/day-5/Jukebox/main.cpp
@@ -8,6 +8,8 @@ int main() {
   Song highway_to_hell("AC/DC", "Highway To Hell", 4.5);
   highway_to_hell.add_rating(3);
   highway_to_hell.add_rating(4);
-  cout << highway_to_hell.get_average_rating();
+  cout << highway_to_hell.get_average_rating() << endl;
+  highway_to_hell.rock();
+  cout << highway_to_hell.get_genre() << endl;
   return 0;
 }
