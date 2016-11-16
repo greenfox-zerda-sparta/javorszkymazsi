07e208e3b3b5d9256878e4efd4ed79c8147d086f
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2016-11-16 15:38:37 +0100

    vannak dolgok, amiket tud

--- week-05/day-3/todo/main.cpp
+++ week-05/day-3/todo/main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <fstream>
 
 using namespace std;
 
 void show_usage(string name);
 
+string get_content(string filename);
+
 int main(int argc, char* argv[]) {
-  //ofstream todo_file("todo.txt");
-  string arg_1 = argv[1];
-  if (argc == 1) {
+  if (argc < 2) {
     show_usage(argv[0]);
-  } else if (arg_1 == "-l") {
-    cout << "listázz, köcsög!" << endl;
-  } else if (arg_1 == "-a") {
-    cout << "adj hozzá valamit" << endl;
-  } else if (arg_1 == "-r"){
-    cout << "törlünk valamit" << endl;
-  } else if (arg_1 == "-c") {
-    cout << "kész ez a task" << endl;
+    return 1;
   }
 
+  string arg_1 = argv[1];
+  if (argc == 2 && arg_1 == "-l") {
+    cout << get_content("todo.txt") << endl;
+  } else if (argc == 2 && arg_1 == "-a") {
+    cout << "adj hozzá valamit" << endl;
+  } else if (argc == 2 && arg_1 == "-r"){
+    cout << "törlünk valamit" << endl;
+  } else if (argc == 2 && arg_1 == "-c") {
+    cout << "kész ez a task" << endl;
+  }
   return 0;
 }
 
@@ -33,4 +36,15 @@ void show_usage(string name) {
        << "-a  Adds a new task" << endl
        << "-r  Removes a task" << endl
        << "-c  Completes a task" << endl;
+}
+
+string get_content(string filename) {
+  ifstream todo_file(filename);
+  string content = "";
+  string buffer;
+  while (getline(todo_file, buffer)) {
+    content += buffer + "\n";
+  }
+  todo_file.close();
+  return content;
 }
