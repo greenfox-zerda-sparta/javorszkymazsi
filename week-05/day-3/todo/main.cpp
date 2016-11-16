ea8af48ce40a4c1d3c91f408dd4a9a2203a4602e
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2016-11-16 16:30:33 +0100

    more beautiful print_content function

--- week-05/day-3/todo/main.cpp
+++ week-05/day-3/todo/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
 void show_usage(string name);
 
-string get_content(string filename);
+string print_content(string filename);
 
 int main(int argc, char* argv[]) {
+  //ofstream todo_file("todo.txt");
   if (argc < 2) {
     show_usage(argv[0]);
     return 1;
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
 
   string arg_1 = argv[1];
   if (argc == 2 && arg_1 == "-l") {
-    cout << get_content("todo.txt") << endl;
+    cout << print_content("todo.txt") << endl;
   } else if (argc == 2 && arg_1 == "-a") {
     cout << "adj hozzá valamit" << endl;
   } else if (argc == 2 && arg_1 == "-r"){
@@ -24,6 +26,7 @@ int main(int argc, char* argv[]) {
   } else if (argc == 2 && arg_1 == "-c") {
     cout << "kész ez a task" << endl;
   }
+  //todo_file.close();
   return 0;
 }
 
@@ -38,12 +41,18 @@ void show_usage(string name) {
        << "-c  Completes a task" << endl;
 }
 
-string get_content(string filename) {
+string print_content(string filename) {
   ifstream todo_file(filename);
   string content = "";
   string buffer;
+  int line_counter = 1;
   while (getline(todo_file, buffer)) {
-    content += buffer + "\n";
+    stringstream ss;
+    ss << line_counter;
+    string str;
+    ss >> str;
+    content += str + " - " + buffer + "\n";
+    ++line_counter;
   }
   todo_file.close();
   return content;
