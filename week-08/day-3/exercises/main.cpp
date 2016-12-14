4aeb72e1a941b65ec4e6e02724ff5175d6624e8f
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2016-12-14 16:01:34 +0100

    darken and lighten working

--- week-08/day-3/exercises/main.cpp
+++ week-08/day-3/exercises/main.cpp
@@ -1,22 +1,28 @@
 #include "Color.h"
 
 int main() {
-  Color c1(200, 25, 0);
-  std::cout << (int)c1.get_red() << std::endl;
-  std::cout << (int)c1.get_green() << std::endl;
-  std::cout << (int)c1.get_blue() << std::endl;
-  Color c2(100, 10, 10);
-  Color c3(c1.blend(c2));
-  std::cout << (int)c3.get_red() << std::endl;
-  std::cout << (int)c3.get_green() << std::endl;
-  std::cout << (int)c3.get_blue() << std::endl;
-  c3.darken(0.2);
-  std::cout << (int)c3.get_red() << std::endl;
-  std::cout << (int)c3.get_green() << std::endl;
-  std::cout << (int)c3.get_blue() << std::endl;
-  c3.lighten(.8);
-  std::cout << (int)c3.get_red() << std::endl;
-  std::cout << (int)c3.get_green() << std::endl;
-  std::cout << (int)c3.get_blue() << std::endl;
+  try {
+    Color c1(200, 25, 0);
+    std::cout << (int)c1.get_red() << std::endl;
+    std::cout << (int)c1.get_green() << std::endl;
+    std::cout << (int)c1.get_blue() << std::endl;
+    Color c2(100, 10, 10);
+    Color c3(c1.blend(c2));
+    std::cout << (int)c3.get_red() << std::endl;
+    std::cout << (int)c3.get_green() << std::endl;
+    std::cout << (int)c3.get_blue() << std::endl;
+    c3.darken(0.2);
+    std::cout << (int)c3.get_red() << std::endl;
+    std::cout << (int)c3.get_green() << std::endl;
+    std::cout << (int)c3.get_blue() << std::endl;
+    c3.lighten(.8);
+    std::cout << (int)c3.get_red() << std::endl;
+    std::cout << (int)c3.get_green() << std::endl;
+    std::cout << (int)c3.get_blue() << std::endl;
+    c3.darken(3);
+  } catch (const char* ex) {
+    std::cout << ex;
+  }
+
   return 0;
 }
